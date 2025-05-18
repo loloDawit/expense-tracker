cfcf36eac98d9368cac68a5639f517ac797021dd
Dawit Abera <dawitabera64@gmail.com>
2025-05-18 11:52:30 -0700

    initial setup

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,18 @@
 // https://docs.expo.dev/guides/using-eslint/
 const { defineConfig } = require('eslint/config');
 const expoConfig = require('eslint-config-expo/flat');
+const prettierPlugin = require('eslint-plugin-prettier');
 
 module.exports = defineConfig([
   expoConfig,
+  {
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: {
+      'prettier/prettier': 'error', // Show Prettier formatting issues as ESLint errors
+    },
+  },
   {
     ignores: ['dist/*'],
   },
