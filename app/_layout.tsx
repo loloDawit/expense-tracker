56530ba2df0f1112133d76ee5a7b7715408628e2
lolodawit <dawitabera64@gmail.com>
2025-05-23 11:35:04 -0700

    Initialize Sentry for error tracking and logging in Layout component

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,35 @@
 import { AuthProvider } from '@/contexts/AuthContext';
+import logger from '@/utils/logger';
+import * as Sentry from '@sentry/react-native';
 import { Stack } from 'expo-router';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 
+Sentry.init({
+  dsn: process.env.EXPO_PUBLIC_SENTRY_DSN,
+
+  enableNative: true,
+  debug: __DEV__,
+
+  // Optional advanced config
+  sendDefaultPii: true,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1,
+  integrations: [
+    Sentry.reactNativeTracingIntegration(),
+    Sentry.feedbackIntegration(),
+  ],
+});
+
 const Layout = () => {
+  useEffect(() => {
+    ErrorUtils.setGlobalHandler((error: any, isFatal?: boolean) => {
+      logger.error('🔥 Global error caught:', error, `Fatal: ${isFatal}`);
+      Sentry.captureException(error);
+      logger.remote?.(error, { isFatal });
+    });
+  }, []);
+
   return (
     <AuthProvider>
       <Stack screenOptions={{ headerShown: false }} />
