830ad2b1f01dc8bd6f743c2fa4be71ef735bd842
lolodawit <dawitabera64@gmail.com>
2025-05-24 22:39:17 -0700

    feat: refactor Layout component to Home with user authentication and tab navigation

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,8 +1,19 @@
+import CustomTabs from '@/components/CustomTabs';
+import { useAuth } from '@/contexts/AuthContext';
 import { Tabs } from 'expo-router';
 import React from 'react';
 
-const Layout = () => {
-  return <Tabs></Tabs>;
+const Home = () => {
+  const { user } = useAuth();
+  console.log('user', user);
+  return (
+    <Tabs tabBar={CustomTabs} screenOptions={{ headerShown: false }}>
+      <Tabs.Screen name="home" />
+      <Tabs.Screen name="statistics" />
+      <Tabs.Screen name="wallet" />
+      <Tabs.Screen name="profile" />
+    </Tabs>
+  );
 };
 
-export default Layout;
+export default Home;
